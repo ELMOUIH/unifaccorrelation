71b239f100605e1e1d43d8e4f8173e0a90f3c622
ELMOUIH <63758189+ELMOUIH@users.noreply.github.com>
2020-04-16 02:10:37 +0200

    Update CodeAbdelkrimElmouih.m

--- CodeAbdelkrimElmouih.m
+++ CodeAbdelkrimElmouih.m
@@ -1,15 +1,16 @@
-%Mini projet ìEstimation des paramËtres du modËles thermodynamique UNIFAC appliquÈ en transfert de matiËreî
-% Question 1 : DÈtermination des paramËtres a12,a21 dans le cas de diffusion de MÈthanol (A) dans líeau(B) ‡ T=313,13K avec Xa=0,35.
+%Mini projet ‚ÄúEstimation des param√®tres du mod√®les thermodynamique UNIFAC appliqu√© en transfert de mati√®re‚Äù
+% Question 1 : D√©termination des param√®tres a12,a21 dans le cas de diffusion de M√©thanol (A) dans l‚Äôeau(B) √† T=313,13K avec Xa=0,35.
 R=[1.4311   0.92]; 
 q=[1.432   1.4];
 Dab0=[2.1*10^(-5)   2.67*10^(-5)]; %Dab0ab et Dab0ba.
-DabExpab=1.33*10^-5; %la valeur expÈrimentale.
-Lam=[R(1).^(1/3)   R(2).^(1/3)]; %LambdaA et Lambda 2.
+DabExpab=1.33*10^-5; %la valeur exp√©rimentale.
+Lam=[R(1).^(1/3)   R(2).^(1/3)]; %Lambda 1 et Lambda 2.
 X=[0.35   1-0.35]; %Fractions molaires methanol / eau.
 T=313.13; 
 TET= [(X(1)*q(1))/(X(1)*q(1)+X(2)*q(2))   (X(2)*q(2))/(X(1)*q(1)+X(2)*q(2))]; % TETA(i)
 Phi=[(X(1)*Lam(1))/(X(1)*Lam(1)+X(2)*Lam(2))   (X(2)*Lam(2))/(X(1)*Lam(1)+X(2)*Lam(2))]; %PHI(i)
-% TO(ij) et TETA(ij) sont citÈs directement dans l'equation ci dessous :
+% TO(ij) et TETA(ij) sont cit√©s directement dans l'equation ci dessous :
+%les boucle sont utilises pour avoir la plus divergence possible
 for i=0 : 100 : 1000
     for j=0 : 100 : 1000
 fun=@(A) abs(exp(X(2)*log(Dab0(1))+X(1)*Dab0(2)+2*(X(1)*log(X(1)./Phi(1))+X(2)*log(X(2)./Phi(2)))+...
@@ -30,16 +31,16 @@ if fun(A)<min
 end
     end
 end
-fprintf('les paramËtres A12 et A21 estimÈs sont :')
+fprintf('les param√®tres A12 et A21 estim√©s sont :')
 A = Aopt
-fprintf('L ecart entre la valeur expÈrimentale et thÈorique (en pourcentage) :')
+fprintf('L ecart entre la valeur exp√©rimentale et th√©orique (en pourcentage) :')
 Ecart = 100*min/DabExpab 
-%ce programme est destinÈ de retrouver le petit ecart exist entre la
-%valeur expÈrimentale et thÈorique, autrement dit la fonction "fminsearch"
-%utilisÈ ici vas etre minimisÈ la valeur absolue entre ces dernieres.
+%ce programme est destin√© de retrouver le petit ecart exist entre la
+%valeur exp√©rimentale et th√©orique, autrement dit la fonction "fminsearch"
+%utilis√© ici vas etre minimis√© la valeur absolue entre ces dernieres.
 
 
-%TraÁage de la variation du coefficient de diffusion en fonction du fraction molaire jusqu'‡ : Xa=0,7.
+%Tra√ßage de la variation du coefficient de diffusion en fonction du fraction molaire jusqu'√† : Xa=0,7.
 
 TO=[exp(-A(1)/T) exp(-A(2)/T) 1 1]; 
 TETA=[(TET(1)*TO(1))/(TET(1)*TO(1)+TET(2)*TO(4)) (TET(2)*TO(2))/(TET(1)*TO(3)+TET(2)*TO(2))...
